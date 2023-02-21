1a8a878a9574be642b2106cfb2f40051485f34f5
Lewis Davies <lewis2106@gmail.com>
2023-02-21 16:30:01 +0000

    Fix variable name & error message

--- macros/ref.sql
+++ macros/ref.sql
@@ -30,13 +30,13 @@
     {% endif %}
 
     {# Raise error if neither the upstream database or schema are set #}
-    {% if prod_database is none and prod_schema is none and not custom_schemas %}
+    {% if prod_database is none and prod_schema is none and not env_schemas %}
         {% set error_msg -%}
 upstream_prod is enabled but at least one required variable is missing.
 Please set at least one of the following variables to correctly configure the package:
 - upstream_prod_database
 - upstream_prod_schema
-- upstream_prod_custom_schemas
+- upstream_prod_env_schemas
 
 The package can be disabled by setting the variable upstream_prod_enabled = False.
         {%- endset %}

c2591d664bcbfd113b0f274243bf3eedb7f4c819
Lewis Davies <lewis2106@gmail.com>
2023-05-11 19:51:33 +0100

    Fix schema for prod target

--- integration_tests/macros/get_custom_schema.sql
+++ integration_tests/macros/get_custom_schema.sql
@@ -1,5 +1,5 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-    {% if var('upstream_prod_env_schemas', False) %}
+    {% if var('upstream_prod_env_schemas', False) and target.name != 'prod' %}
         {{ generate_schema_name_for_env(custom_schema_name, node) }}
     {% else %}
         {{ adapter.dispatch('generate_schema_name', 'dbt')(custom_schema_name, node) }}

6ddc1157a4e883341a5c27f4f7751c0764d023b7
Lewis Davies <lewis2106@gmail.com>
2023-06-20 20:12:21 +0100

    Add versioned model tests

--- integration_tests/dev_sch/macros/ref.sql
+++ integration_tests/dev_sch/macros/ref.sql
@@ -4,9 +4,10 @@
     prod_schema=var("upstream_prod_schema", None),
     enabled=var("upstream_prod_enabled", True),
     fallback=var("upstream_prod_fallback", False),
-    env_schemas=var("upstream_prod_env_schemas", False)
+    env_schemas=var("upstream_prod_env_schemas", False),
+    version=None
 ) %}
 
-    {% do return(upstream_prod.ref(parent_model, prod_database, prod_schema, enabled, fallback, env_schemas)) %}
+    {% do return(upstream_prod.ref(parent_model, prod_database, prod_schema, enabled, fallback, env_schemas, version)) %}
 
 {% endmacro %}

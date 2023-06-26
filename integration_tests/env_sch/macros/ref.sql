3c54f9739a5bff908f148105d618a2a7c3094330
Lewis Davies <lewis2106@gmail.com>
2023-06-26 14:12:01 +0100

    Add prefer_recent tests

--- integration_tests/env_sch/macros/ref.sql
+++ integration_tests/env_sch/macros/ref.sql
@@ -5,7 +5,8 @@
     enabled=var("upstream_prod_enabled", True),
     fallback=var("upstream_prod_fallback", False),
     env_schemas=var("upstream_prod_env_schemas", False),
-    version=None
+    version=None,
+    prefer_recent=var("upstream_prod_prefer_recent", False)
 ) %}
 
     {% do return(upstream_prod.ref(parent_model, prod_database, prod_schema, enabled, fallback, env_schemas, version)) %}

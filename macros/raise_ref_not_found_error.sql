c5e83f5548e4fe5cf17bd6f1d856f4001a6564b6
Lewis Davies <lewis2106@gmail.com>
2023-08-04 08:49:49 +0100

    Fix typo

--- macros/raise_ref_not_found_error.sql
+++ macros/raise_ref_not_found_error.sql
@@ -8,7 +8,7 @@
 [{{ current_model }}] upstream_prod couldnt find the specified model:
 
 DATABASE: {{ relation.database }}
-SCHEMA:   {{ realtion.schema }}
+SCHEMA:   {{ relation.schema }}
 RELATION: {{ relation.identifier }}
 
 Check your variable settings in the README or create a GitHub issue for more help.

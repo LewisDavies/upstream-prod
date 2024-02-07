34eee4d04a5a3f35d287bb34ada1ae72ad56e15a
Lewis Davies <lewis2106@gmail.com>
2024-02-07 11:29:50 +0000

    Add Databricks support

--- macros/get_table_update_ts.sql
+++ macros/get_table_update_ts.sql
@@ -3,21 +3,44 @@
 {% endmacro %}
 
 {% macro default__get_table_update_ts(relation) %}
-    -- This is intentionally blank to avoid an error on non-compatible databases
-{% endmacro %}
 
-{% macro snowflake__get_table_update_ts(relation) %}
-
-    {% set table_info_query %}
-        select
-            last_altered 
-        from
-            {{ relation.database }}.information_schema.tables
-        where
-            table_catalog = upper('{{ relation.database }}')
-            and table_schema = upper('{{ relation.schema }}')
-            and table_name = upper('{{ relation.identifier }}')
-    {% endset %}
+    {% if target.type in ("snowflake", "databricks") %}
+        {# Query assumes database objects don't have case-sensitive names #}
+        {% set table_info_query %}
+            select
+                last_altered 
+            from
+                {{ relation.database }}.information_schema.tables
+            where
+                upper(table_catalog) = upper('{{ relation.database }}')
+                and upper(table_schema) = upper('{{ relation.schema }}')
+                and upper(table_name) = upper('{{ relation.identifier }}')
+        {% endset %}
+    {% elif target.type == "bigquery" %}
+        {% set table_info_query %}
+            select
+                coalesce(
+                    timestamp_millis(meta.last_modified_time), 
+                    inf_sch.creation_time
+                ) as last_altered
+            from
+                {{ relation.database }}.{{ relation.schema }}.INFORMATION_SCHEMA.TABLES inf_sch
+                left join {{ relation.database }}.{{ relation.schema }}.__TABLES__ meta
+                    on inf_sch.table_catalog = meta.project_id
+                    and inf_sch.table_schema = meta.dataset_id
+                    and inf_sch.table_name = meta.table_id
+            where
+                inf_sch.table_catalog = '{{ relation.database }}'
+                and inf_sch.table_schema = '{{ relation.schema }}'
+                and inf_sch.table_name = '{{ relation.identifier }}'
+        {% endset %}
+    {% else %}
+        {% set warning_msg %}
+upstream_prod_prefer_recent is set to true but this feature is incompatible with {{ target.type }} databases. Unsetting the variable may improve project performance.
+        {% endset %}
+        {% do exceptions.warn(warning_msg) %}
+        {{ return(None) }}
+    {% endif %}
 
     {% set result = run_query(table_info_query) %}
     {% if result | length == 0 %}
@@ -30,39 +53,5 @@ Multiple matches in information schema for {{ relation }}
     {% else %}
         {{ return(result.rows.values()[0][0] ) }}
     {% endif %}
-    
-{% endmacro %}
 
-{% macro bigquery__get_table_update_ts(relation) %}
-
-    {% set table_info_query %}
-        select
-            coalesce(
-                timestamp_millis(meta.last_modified_time), 
-                inf_sch.creation_time
-            ) as last_altered
-        from
-            {{ relation.database }}.{{ relation.schema }}.INFORMATION_SCHEMA.TABLES inf_sch
-            left join {{ relation.database }}.{{ relation.schema }}.__TABLES__ meta
-                on inf_sch.table_catalog = meta.project_id
-                and inf_sch.table_schema = meta.dataset_id
-                and inf_sch.table_name = meta.table_id
-        where
-            inf_sch.table_catalog = '{{ relation.database }}'
-            and inf_sch.table_schema = '{{ relation.schema }}'
-            and inf_sch.table_name = '{{ relation.identifier }}'
-    {% endset %}
-
-    {% set result = run_query(table_info_query) %}
-    {% if result | length == 0 %}
-        {{ return(None) }}
-    {% elif result | length > 1 %}
-        {% set error_msg %}
-Multiple matches in information schema for {{ relation }}
-        {% endset %}
-        {% do exceptions.raise_compiler_error(error_msg) %}
-    {% else %}
-        {{ return(result.rows.values()[0][0] ) }}
-    {% endif %}
-    
 {% endmacro %}

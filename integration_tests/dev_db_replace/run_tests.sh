802798e34eb507455901e1fc2fa95eb8e7a41c49
Lewis Davies <lewis2106@gmail.com>
2024-04-16 21:03:46 +0100

    Update test script

--- integration_tests/dev_db_replace/run_tests.sh
+++ integration_tests/dev_db_replace/run_tests.sh
@@ -1,18 +1,24 @@
 # Setup
 set -e
-echo "\nSETTING UP ENVIRONMENT\n"
+echo ""
+echo "## SETTING UP ENVIRONMENT"
+echo ""
 dbt clean
 dbt deps
 dbt run-operation create_test_db --args '{db: upstream__prod_db}'
 dbt run-operation create_test_db --args '{db: upstream__dev_db}'
 
 # Create staging model in appropriate envs
-echo "\nBUILDING STAGING MODELS\n"
+echo ""
+echo "## BUILDING STAGING MODELS"
+echo ""
 dbt build -s stg__defer_prod stg__defer_vers stg__dev_newer --target prod
 dbt build -s stg__dev_fallback stg__dev_newer
 
 # Build & test downstream models
-echo "\nBUILDING DOWNSTREAM MODELS\n"
+echo ""
+echo "## BUILDING DOWNSTREAM MODELS"
+echo ""
 dbt build -s models/marts
 
 # Check dbt-codegen compatibility

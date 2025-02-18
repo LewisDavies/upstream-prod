b6251aa68dacc1e54c5a19787a5c0b8de4a86334
Lewis Davies <lewis2106@gmail.com>
2025-02-18 22:36:57 +0000

    Add cross-project ref tests

---
+++ integration_tests/dev_db/models/marts/cross_project.sql
@@ -0,0 +1,10 @@
+select
+    source_target,
+    source_database,
+    source_schema,
+    source_model,
+    '{{ target.name }}' as this_target,
+    '{{ this.database }}' as this_database,
+    '{{ this.schema }}' as this_schema,
+    '{{ this.name }}' as this_model
+from {{ ref('import_project', 'stg__cross_project') }}

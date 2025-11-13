a03e4ab6c3045ceaf81f8086c3ad035cfbe5d6d4
Lewis Davies <lewis2106@gmail.com>
2025-11-13 20:58:02 +0000

    Condense test logs

--- integration_tests/run_tests.sh
+++ integration_tests/run_tests.sh
@@ -2,7 +2,6 @@
 set -e
 echo ""
 echo "## SETTING UP ENVIRONMENT"
-echo ""
 dbt clean
 dbt deps
 dbt run-operation create_test_db --args '{db: upproddb}'
@@ -11,7 +10,6 @@ dbt run-operation create_test_db --args '{db: updevdb}'
 # Create staging models in appropriate envs
 echo ""
 echo "## RUNNING STAGING MODELS"
-echo ""
 dbt snapshot -t prod
 dbt run -s stg__defer_prod stg__defer_vers stg__dev_newer stg__cross_project -t prod
 dbt run -s stg__dev_fallback stg__dev_newer
@@ -19,17 +17,14 @@ dbt run -s stg__dev_fallback stg__dev_newer
 # Build & test downstream models
 echo ""
 echo "## BUILDING DOWNSTREAM MODELS"
-echo ""
 dbt build -s models/marts
 
 # Check --empty flag
 echo ""
 echo "## CHECKING EMPTY FLAG"
-echo ""
 dbt build -s defer_prod --empty
 
 # Check dbt-codegen compatibility
 echo ""
 echo "## CHECKING CODEGEN OUTPUT"
-echo ""
 dbt run-operation generate_model_yaml --args '{"model_names": [stg__defer_prod]}'

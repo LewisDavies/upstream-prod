e08dff457679b6a22e62538dea511c1ca5c373f0
Lewis Davies <lewis2106@gmail.com>
2026-08-27 08:16:20 +0100

    Remove alias check when finding model node

--- macros/find_model_node.sql
+++ macros/find_model_node.sql
@@ -5,8 +5,10 @@
 {% macro default__find_model_node(model, project, version) %}
 
     {% if execute is true %}
+        -- For a while this macro checked the name against model aliases too. This
+        -- caused issues when multiple models in the same project had the same alias.
         {% set matching_nodes = [] %}
-        {% for n in graph.nodes.values() if model in [n["name"], n["alias"]] %}
+        {% for n in graph.nodes.values() if n["name"] == model %}
             {% if project is none or project == n["package_name"] %}
                 {% if version is not none %}
                     {% if n["version"] == version %}
@@ -19,6 +21,18 @@
                 {% endif %}
             {% endif %}
         {% endfor %}
+
+        -- A one-arg ref may match the same model name in more than one package.
+        -- This raises an error to match dbt's behaviour.
+        {% if matching_nodes | length > 1 %}
+            {% set candidate_ids = matching_nodes | map(attribute="unique_id") | join(", ") %}
+            {% do exceptions.raise_compiler_error(
+                "[upstream_prod] ref('" ~ model ~ "') is ambiguous: matched "
+                ~ matching_nodes | length ~ " nodes (" ~ candidate_ids ~ "). "
+                ~ "Use a two-argument ref to specify the package."
+            ) %}
+        {% endif %}
+
         {{ return(matching_nodes | first) }}
     {% endif %}
 
